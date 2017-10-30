682169177485155e9d239664399514db87ec8592
huangzebin <ben.wong@xuebangsoft.com>
2017-10-30 18:54:00 +0800

    头部开发

--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -2,19 +2,39 @@ import React from 'react'
 import {
   Route,
   Switch,
-  Redirect
+  Redirect,
+  Link
 } from 'react-router-dom'
 import Home from 'VIEW/Home/'
 import Setting from 'VIEW/Setting/'
+import './index.css'
+import menus from './menus'
 
 class Main extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path={`${this.props.match.url}`} component={Home}/>
-        <Route exact path='/setting' component={Setting}/>
-        <Redirect to="/" />
-      </Switch>
+      <div>
+        <header>
+          <div className='companyAd'></div>
+          <div className='contact'></div>
+          <div className='navbar_container'>
+            <ul className='navbar'>
+              {menus.map(m => (
+                <li className={this.props.location.pathname === m.url?'active':''}>
+                 <Link to={m.url}>
+                   {m.name}
+                 </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path={`${this.props.match.url}`} component={Home}/>
+          <Route exact path='/setting' component={Setting}/>
+          <Redirect to="/" />
+        </Switch>
+      </div>
     )
   }
 }
---
+++ src/views/Main/menus.js
@@ -0,0 +1,26 @@
+export default [{
+    name: '首页',
+    url: '/'
+  },{
+    name: '公司简介',
+    url: '/setting'
+  },{
+    name: '产品中心',
+    url: '/1'
+  },{
+    name: '店面展示',
+    url: '/2'
+  },{
+    name: '招商加盟',
+    url: '/3'
+  },{
+    name: '新闻资讯',
+    url: '/4'
+  },{
+    name: '经销商专区',
+    url: '/5'
+  },{
+    name: '联系我们',
+    url: '/6'
+  }
+]
